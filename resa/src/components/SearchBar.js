4b7f5a3cfb31b0d3898ece0f5ea19c9d38d6efaf
l_inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-08-05 16:33:49 +0200

    done

--- resa/src/components/SearchBar.js
+++ resa/src/components/SearchBar.js
@@ -5,9 +5,19 @@ function SearchBar({ search, setSearch }) {
     return (
         <Input 
             type="text"
-            placeholder="Search"
+            placeholder="Rechercher"
             value={search} 
             onChange={e => setSearch(e.target.value)}
+            style={{
+                width: '100%', // Fills the width of the container
+                color: '#fff', // Make text color white
+                backgroundColor: '#333', // Make background dark grey
+                borderColor: '#333', // Make border dark grey
+                marginBottom: '20px', // Space out from the form
+                marginTop: '20px', // Space out from the form
+                borderRadius: '15px', // Match the border radius of the form
+                textAlign: 'center', // Center the input text
+            }}
         />
     );
 }
